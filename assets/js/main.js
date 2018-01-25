6363ebaeeea92d0e24dfa77c08888583ad251062
Brandon Vergara <leovergara.dark@gmail.com>
2018-01-24 23:25:28 -0600

    adding text

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,30 @@ let MainController = (function(){
 
  var typed = function(){
    console.log("Typed..");
+   $(".puntos").typed({
+    strings: ["..."],
+    showCursor: false,
+    typeSpeed: 300,
+    backSpeed: 300,
+    loop: true,
+    contentType: 'html'
+    });
+    $(".otro").typed({
+      strings: ["UNA MUJER MÁS QUE HERMOSA.", "MUJER INTELIGENTE", "MUJER QUE CAUTIVA", "MUJER QUE ENCANTA", "CON SENTIMIENTOS INCOMPARABLES",
+      "CON UNA PERSONALIDAD MUY LINDA", "¿Y QUIEN ES LA MUJER QUE HABLO?", "SÓLO PODRÍAS SER UNA...", "ANA KAREN SUÁREZ CASTRO"],
+      typeSpeed: 130,
+      loop: false,
+      contentType: 'html'
+
+  });
+  $(".final").typed({
+    strings: ["!TE QUIERO BONITA¡"],
+    startDelay: 50000,
+    showCursor: false,
+    typeSpeed: 200,
+    loop: false,
+    contentType: 'html'
+    });
    
  };
  var start = function(){
